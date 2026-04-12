e65fcdfca6019443ab014475594b99c12434bdce
Yannick Nkongolo <ynkong1@wgu.edu>
2026-04-11 22:16:21 -0500

    Fix: switch to HTTP API v2 (1nt249f9ha) - correct Lambda event format, fixes CORS + routing

--- Client/portal-page.js
+++ Client/portal-page.js
@@ -190,7 +190,7 @@
         });
     };
 
-    window.RNB_SECTION_API = 'https://73ucgufya3.execute-api.us-east-2.amazonaws.com/update-client-section';
+    window.RNB_SECTION_API = 'https://1nt249f9ha.execute-api.us-east-2.amazonaws.com/update-client-section';
 
     window.savePortalSection = function (section, data, statusEl, btnEl, editAction) {
         btnEl.disabled = true;
